70ff5b629de9b85531fd2eeed977f125c53f7660
HakeemS <33767804+HakeemS@users.noreply.github.com>
2024-04-16 18:12:27 -0400

    WiP; OnSeq SM

--- OLEDPmod.srcs/sources_1/new/onSeq_S.vhd
+++ OLEDPmod.srcs/sources_1/new/onSeq_S.vhd
@@ -44,15 +44,69 @@ entity onSeq_S is
 end onSeq_S;
 
 architecture Behavioral of onSeq_S is
-
+    -- Constant --
+constant c_Delay4us : std_logic_vector(8 downto 0) := x"18F";                -- constant = 399
+    -- State Initialization -- 
+type states is (s0, s1, s2, s3, s4); 
+signal stt: states := s0; 
     -- Signals --
+signal start : std_logic := '0'; 
 signal OLEDPRst_t : std_logic := '1';
 signal OLEDVddc_t : std_logic := '0'; 
 signal OLEDVbat_t : std_logic := '0'; 
-signal OLEDByte_t : std_logic_vector(N-1 downto 0) := (others => '0'); 
+signal OLEDByte_t : std_logic_vector(N-1 downto 0) := (others => '0');
+signal delay4us : std_logic_vector(8 downto 0) := (others => '0');        
+
 
 
 begin
+    trns : process( clk, rst, sw, stt )
+    begin
+        if (rising_edge(clk)) then
+            if (rst = '1') then
+                stt <= s0; 
+                OLEDPRst_t <= '1'; 
+                OLEDVddc_t <= '0'; 
+                OLEDVbat_t <= '0';
+                OLEDByte_t <= (others => '0'); 
+                delay4us <= (others => '0'); 
+            else
+                case( stt ) is
+                    when s0 =>
+                        if (start = '1') then
+                            stt <= s1; 
+                        end if ;
+                    when s1 =>
+                    when s2 =>
+                    when others =>
+                
+                end case ;
+            end if; 
+        end if ;
+    end process ; -- trns
+
+    output : process( clk, stt )
+    begin
+        case( stt ) is
+            when s0 =>
+                if (sw = '1') then
+                    start <= '1'; 
+                    OLEDPRst_t <= '0'; 
+                end if ;
+            when s1 => 
+                    if (rising_edge(clk)) then
+                        if (delay4us = c_Delay4us) then
+                            OLEDPRst_t <= '1'; 
+                        else 
+                            delay4us <= std_logic_vector(unsigned(delay4us) + 1); 
+                        end if ;
+                        
+                    end if ; 
+
+            when others =>
+        
+        end case ;
+    end process ; -- output
 
 
     -- Signals to OUT -- 
