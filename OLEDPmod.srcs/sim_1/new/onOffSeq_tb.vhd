c9ab638acdafa2626c62e039ab73f8ec27e966dd
HakeemS <33767804+HakeemS@users.noreply.github.com>
2024-04-27 10:52:06 -0400

    WiP; Off Seq Started (Post Fixes; git reset --soft)

---
+++ OLEDPmod.srcs/sim_1/new/onOffSeq_tb.vhd
@@ -0,0 +1,99 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/16/2024 07:18:51 PM
+-- Design Name: 
+-- Module Name: onOffSeq_tb - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity onOffSeq_tb is
+--  Port ( );
+end onOffSeq_tb;
+
+architecture Behavioral of onOffSeq_tb is
+
+    --UUT/DUT -- 
+component onSeq_S is 
+    generic (N : integer := 8); 
+    port (  clk : in std_logic; 
+            rst : in std_logic;
+            sw : in std_logic; 
+            OLEDPRst : out std_logic; 
+            OLEDVddc : out std_logic; 
+            OLEDVbat : out std_logic; 
+            OLEDRdy : out std_logic; 
+            OLEDByte : out std_logic_vector(N-1 downto 0)
+    );
+end component; 
+    -- Generic Constants -- 
+constant N : integer := 8;  
+    -- Signals --
+signal clk, rst, sw, OLEDPRst, OLEDVddc, OLEDVbat, OLEDRdy : std_logic; 
+signal OLEDByte : std_logic_vector(N-1 downto 0); 
+    -- clk Signals --
+signal clk_period : time := 10ns; 
+signal clk_stop : boolean; 
+    
+
+begin
+    clkproc : process 
+    begin
+        clk <= '1';  
+        wait for clk_period/2; 
+        clk <= '0'; 
+        wait for clk_period/2; 
+    end process ; -- clkproc
+
+    UUT : onSeq_s 
+    generic map (
+        N => 8
+    ) 
+    port map (
+        clk => clk, 
+        rst => rst, 
+        sw => sw, 
+        OLEDPRst => OLEDPRst, 
+        OLEDVddc => OLEDVddc, 
+        OLEDVbat => OLEDVbat, 
+        OLEDRdy => OLEDRdy, 
+        OLEDByte => OLEDByte
+    ); 
+
+    stim : process 
+    begin
+        sw <= '0'; 
+        rst <= '0'; 
+        
+        wait for 20ns; 
+
+        sw <= '1'; 
+    
+        wait;  
+    end process ; -- stim
+
+end Behavioral;
--- OLEDPmod.srcs/sources_1/new/onSeq_S.vhd
+++ OLEDPmod.srcs/sources_1/new/onSeq_S.vhd
@@ -21,6 +21,7 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;                                                   -- used for incrementing vector (unsigned())
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -39,45 +40,71 @@ entity onSeq_S is
             OLEDPRst : out std_logic; 
             OLEDVddc : out std_logic; 
             OLEDVbat : out std_logic; 
+            OLEDRdy : out std_logic; 
             OLEDByte : out std_logic_vector(N-1 downto 0)
     );
 end onSeq_S;
 
 architecture Behavioral of onSeq_S is
     -- Constant --
-constant c_Delay4us : std_logic_vector(8 downto 0) := x"18F";                -- constant = 399
+constant c_Delay4us : std_logic_vector(11 downto 0) := x"18F";                       -- constant = 399
+constant c_Delay200ms :  std_logic_vector(27 downto 0) := x"1312CFF";               -- constant = 19,999,99 
     -- State Initialization -- 
-type states is (s0, s1, s2, s3, s4); 
-signal stt: states := s0; 
+type states is (idle, rstStt, s0, s1, s2, s3, s4);                                        
+signal stt: states := idle; 
     -- Signals --
-signal start : std_logic := '0'; 
+signal powerOn : std_logic := '0'; 
+signal powerOff : std_logic := '0'; 
+signal running : std_logic := '0'; 
 signal OLEDPRst_t : std_logic := '1';
 signal OLEDVddc_t : std_logic := '0'; 
 signal OLEDVbat_t : std_logic := '0'; 
+signal OLEDRdy_t : std_logic := '0'; 
 signal OLEDByte_t : std_logic_vector(N-1 downto 0) := (others => '0');
-signal delay4us : std_logic_vector(8 downto 0) := (others => '0');        
+signal delay4us : std_logic_vector(11 downto 0) := (others => '0');      
+signal delay200ms : std_logic_vector(27 downto 0) := (others => '0');   
 
 
 
 begin
-    trns : process( clk, rst, sw, stt )
+    trns : process( clk, rst, sw, stt, powerOn, powerOff, running, OLEDPRst_t, OLEDVddc_t, OLEDVbat_t, OLEDByte_t, 
+                    OLEDRdy_t  )
     begin
         if (rising_edge(clk)) then
             if (rst = '1') then
-                stt <= s0; 
-                OLEDPRst_t <= '1'; 
-                OLEDVddc_t <= '0'; 
-                OLEDVbat_t <= '0';
-                OLEDByte_t <= (others => '0'); 
-                delay4us <= (others => '0'); 
+                stt <= rstStt; 
             else
                 case( stt ) is
+                    when rstStt => stt <= idle; 
+                    when idle =>
+                        if (powerOn = '1') then
+                            stt <= s0; 
+                        --elsif (powerOff = '1') then
+                        --   
+                        else
+                            stt <= idle;  
+                        end if ;
                     when s0 =>
-                        if (start = '1') then
+                        if (OLEDPRst_t = '1') then
                             stt <= s1; 
+                        else
+                            stt <= s0; 
                         end if ;
                     when s1 =>
-                    when s2 =>
+                        if (OLEDVddc_t = '1' and OLEDVbat_t = '1') then
+                            stt <= s2;
+                        else
+                            stt <= s1; 
+                        end if ;
+                    when s2 => stt <= s3; 
+                    when s3 => 
+                        if (OLEDRdy_t = '1') then
+                            stt <= idle; 
+                        --elsif () then
+                            
+                        else
+                            stt <= s3; 
+                        end if ;
                     when others =>
                 
                 end case ;
@@ -85,24 +112,66 @@ begin
         end if ;
     end process ; -- trns
 
-    output : process( clk, stt )
+    output : process( clk, stt, sw, powerOn, powerOff, running, OLEDPRst_t, OLEDVddc_t, OLEDVbat_t, OLEDByte_t, 
+                        OLEDRdy_t, delay4us, delay200ms )
     begin
         case( stt ) is
-            when s0 =>
-                if (sw = '1') then
-                    start <= '1'; 
+            when rstStt => 
+                powerOn <= '0'; 
+                powerOff <= '0';
+                running <= '0'; 
+                OLEDPRst_t <= '1'; 
+                OLEDVddc_t <= '0'; 
+                OLEDVbat_t <= '0';
+                OLEDRdy_t <= '0'; 
+                OLEDByte_t <= (others => '0'); 
+                delay4us <= (others => '0'); 
+                delay200ms <= (others => '0'); 
+            when idle =>
+                delay4us <= (others => '0'); 
+                delay200ms <= (others => '0'); 
+                if (sw = '1' and running = '0') then
+                    powerOn <= '1'; 
+                    powerOff <= '0'; 
+                    running <= '1'; 
                     OLEDPRst_t <= '0'; 
+                --elsif (sw = '0' and running = '1') then
+                --    powerOn <= '0'; 
+                --    powerOff <= '1'; 
+                --    running <= '0'; 
+                --    OLEDPRst_t <= '0'; 
+                end if ;
+            when s0 => 
+                if (rising_edge(clk)) then
+                    if (delay4us = c_Delay4us) then
+                        OLEDPRst_t <= '1'; 
+                        delay4us <= (others => '0'); 
+                    else 
+                        delay4us <= std_logic_vector(unsigned(delay4us) + 1); 
+                    end if ;
                 end if ;
             when s1 => 
-                    if (rising_edge(clk)) then
-                        if (delay4us = c_Delay4us) then
-                            OLEDPRst_t <= '1'; 
-                        else 
-                            delay4us <= std_logic_vector(unsigned(delay4us) + 1); 
-                        end if ;
-                        
-                    end if ; 
-
+                if (rising_edge(clk)) then
+                    if (delay4us = c_Delay4us) then
+                        OLEDVddc_t <= '1'; 
+                        OLEDVbat_t <= '1'; 
+                        delay4us <= (others => '0'); 
+                    else 
+                        delay4us <= std_logic_vector(unsigned(delay4us) + 1); 
+                    end if ;
+                end if ;
+            when s2 => 
+                OLEDByte_t <= x"AF";                                                -- display on command 
+            when s3 => 
+                if (rising_edge(clk)) then
+                    if (delay200ms = c_Delay200ms) then
+                        OLEDRdy_t <= '1';
+                        delay200ms <= (others => '0' ) ;    
+                        powerOn <= '0';                                    
+                    else 
+                        delay200ms <= std_logic_vector(unsigned(delay200ms) + 1); 
+                    end if ;
+                end if ;
             when others =>
         
         end case ;
@@ -113,5 +182,6 @@ begin
     OLEDPRst <= OLEDPRst_t; 
     OLEDVddc <= OLEDVddc_t; 
     OLEDVbat <= OLEDVbat_t;
+    OLEDRdy <= OLEDRdy_t; 
     OLEDByte <= OLEDByte_t; 
 end Behavioral;
