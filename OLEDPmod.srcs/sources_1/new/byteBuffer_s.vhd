2189184d9266534434a49e234b2f14a335a2741f
HakeemS <33767804+HakeemS@users.noreply.github.com>
2024-05-10 09:51:27 -0400

    WiP; trns process complete

--- OLEDPmod.srcs/sources_1/new/byteBuffer_s.vhd
+++ OLEDPmod.srcs/sources_1/new/byteBuffer_s.vhd
@@ -21,6 +21,7 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 library myLib; 
 use myLib.types_p.all;                                                                  -- used for array of bytes to be used as IN
@@ -39,7 +40,7 @@ entity byteBuffer_s is
     port (  clk : in std_logic;  
             rst : in std_logic; 
             startRdy : in std_logic;                                                    -- indicates all bytes recieved; Ready to start Tx  
-            byteFlag : in std_logic;                                                    -- byte(s) have are ready to be captured in buffer
+            byteFlag : in std_logic;                                                    -- byte(s) are ready to be captured in buffer
             TxReady : in std_logic;                                                     -- SPI_Tx ready to Tx 
             nxByte : in std_logic;                                                      -- SPI_Tx ready for next byte in current data/command sequence 
             byteCountIN : in std_logic_vector(3 downto 0);                              -- number of bytes to Tx 
@@ -48,20 +49,84 @@ entity byteBuffer_s is
             startOUT : out std_logic;                                                   -- start signal sent to SPI_Tx
             DCOUT: out std_logic;                                                       -- current data/command bit to be Tx
             byteCountOUT : out std_logic_vector(3 downto 0); 
-            byteOUT : out std_logic_vector(7 downto 0)                                  -- current bit to be Tx
+            byteOUT : out std_logic_vector(N-1 downto 0)                                -- current byte to be Tx
     );
 end byteBuffer_s;
 
 architecture Behavioral of byteBuffer_s is
 
     -- State Initialization --
-type state is (rstStt, idle, load, send); 
+type state is (rstStt, idle, lx, sx);                                                   -- lx = ld = load; sx = snd = send 
 signal stt : state := idle;  
     -- Signals --
-signal bytesIN_i : byteArr (9 downto 0);                                                -- For now default max number of bytes stored is 10; 
+signal lxFlag : std_logic := '0'; 
+signal lxDone : std_logic := '0';     
+signal sxDone : std_logic := '0';                                                   
+signal startOUT_t : std_logic := '0'; 
+signal DCOUT_t : std_logic := '0'; 
+signal byteCountIN_i : std_logic_vector(3 downto 0) := (others => '0'); 
+signal byteCountOUT_t : std_logic_vector(3 downto 0) := (others => '0');
+signal byteOUT_t : std_logic_vector(N-1 downto 0) := (others => '0'); 
+signal DCIN_i : std_logic_vector(9 downto 0) := (others => '0');                    -- since default max bytes stored is 10, 10 bit vector to store each of the accompanying D/C bits 
+signal bytesIN_i : byteArr (9 downto 0) := (others => (others => '0'));                 -- For now default max number of bytes stored is 10; 
 
 
 begin
+    trns : process( clk, rst, stt )
+    begin
+        if (rising_edge(clk)) then
+            if (rst = '1') then
+                stt <= rstStt; 
+            else
+                case( stt ) is
+                    when rstStt => stt <= idle;
+                    when idle =>
+                        if (lxFlag = '1') then
+                            stt <= lx; 
+                        else
+                            stt <= idle; 
+                        end if ;
+                    when lx =>
+                        if (lxDone = '1') then
+                            stt <= sx; 
+                        else
+                            stt <= lx; 
+                        end if ;                               
+                    when others =>
+                        if (sxDone = '1') then
+                            if (byteFlag = '1') then
+                                stt <= lx; 
+                            else
+                                stt <= idle; 
+                            end if ;
+                        else
+                            stt <= sx; 
+                        end if ;
+                end case ;
+            end if ;
+        end if ;
+        
+    end process ; -- trns
 
-
+    output : process( clk, rst, stt )
+    begin
+        if (rising_edge(clk)) then
+            case( stt ) is
+                when rstStt =>
+                    lxDone <= '0'; 
+                    sxDone <= '0';
+                    startOUT_t <= '0'; 
+                    DCOUT_t <= '0'; 
+                    byteCountIN_i <= (others => '0'); 
+                    byteCountOUT_t <= (others => '0');
+                    byteOUT_t <= (others => '0');
+                    DCIN_i <= (others => '0'); 
+                    bytesIN_i <= (others => (others => '0'));
+                when idle =>
+                when lx =>
+                when others =>
+            
+            end case ;
+        end if ;
+    end process ; -- output
 end Behavioral;
